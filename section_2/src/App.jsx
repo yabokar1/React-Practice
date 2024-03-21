ce54c1b72edc4b9bfb6f1e02b5494dc40122cbd6
yabokar1 <yabokar@gmail.com>
2024-03-21 17:02:35 -0400

    Added css to player component

--- section_2/src/App.jsx
+++ section_2/src/App.jsx
@@ -1,8 +1,17 @@
+import Player from "./components/Player.jsx";
+
 function App() {
   return (
-    <h1 className="font-caprasimo text-5xl mx-auto text-[#3f3b00] mt-3 ml-3">
-      React Tic-Tac-Toe
-    </h1>
+    <main>
+      <div className="max-w-3xl my-12 mx-auto p-8 rounded-md bg-gradient-to-r from-[#383624] to-[#282617] shadow-lg relative">
+        <ol className=" flex justify-center items-center gap-8 list-none p-0 my-4 mx-0">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   );
 }
 
--- section_2/src/components/Player.jsx
+++ section_2/src/components/Player.jsx
@@ -6,19 +6,36 @@ export default function Player({ name, symbol }) {
     setIsEditing(true);
   }
 
-  let playerName = <span className="">{name}</span>;
+  let playerName = (
+    <span className="inline-flex w-40 text-base text-[#e1dec7] uppercase m-0 p-2 rounded-md truncate text-center">
+      {name}
+    </span>
+  );
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = (
+      <input
+        className="font-inherit text-base w-40 border-none py-2 px-2 animate-pulse bg-[#46432f] text-center uppercase"
+        type="text"
+        required
+      />
+    );
   }
 
   return (
-    <li>
-      <span>
+    <li className=" flex items-center w-[50%] border-2 border-solid border-transparent active:border-[#f6e35a] active:animate-pulse">
+      <span className="border-2 border-transparent p-2 rounded-md font-bold">
         {playerName}
-        <span className="">{symbol}</span>
+        <span className=" ml-4 text-base text-[#e1dec7] active:text-[#f6e35a]">
+          {symbol}
+        </span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button
+        className="w-12 border-none bg-transparent text-[#c3ba78] text-sm cursor-pointer transition-colors duration-200 py-2 px-1 text-center hover:text-[#f8ca31]"
+        onClick={handleEditClick}
+      >
+        Edit
+      </button>
     </li>
   );
 }
